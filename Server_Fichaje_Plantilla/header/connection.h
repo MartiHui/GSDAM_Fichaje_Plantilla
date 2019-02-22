a83a6bce094938bb9de4b1d574e28e562411e29d
MartiHui <martiyanghui@gmail.com>
2019-02-22 14:21:09 +0100

    CHANGE Ligeros cambios en el estilo del código

--- Server_Fichaje_Plantilla/header/connection.h
+++ Server_Fichaje_Plantilla/header/connection.h
@@ -8,6 +8,7 @@ class QWebSocket;
 // La clase Connection almacena la información de las conexiones recibidas por el servidor
 class Connection : public QObject {
     Q_OBJECT
+
 public:
     explicit Connection(QWebSocket *webSocket);
     ~Connection();
--- Server_Fichaje_Plantilla/source/main.cpp
+++ Server_Fichaje_Plantilla/source/main.cpp
@@ -2,8 +2,7 @@
 
 #include "connectionmanager.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
     ConnectionManager::getInstance(1234);
