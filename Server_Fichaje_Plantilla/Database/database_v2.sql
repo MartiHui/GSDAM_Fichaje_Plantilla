a9b00945d96e5c3fccb367c2b06bc0c5d5097469
MartiHui <martiyanghui@gmail.com>
2019-03-07 13:26:06 +0100

    ADD Nueva base de datos
    Se hacen varios cambios para reflejar las modificaciones que se van a
    realizar en el proyecto. La tabla de empleados guarda nombre y apellidos
    y la tabla de fichajes tiene registros separados para entrada y salida,
    con un boolean par saber si el registro corresponde a una salida o una
    entrada.

---
+++ Server_Fichaje_Plantilla/Database/database_v2.sql
@@ -0,0 +1,239 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.6
+-- Dumped by pg_dump version 10.6
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: admins; Type: TABLE; Schema: public; Owner: usuario
+--
+
+CREATE TABLE public.admins (
+    admin_id integer NOT NULL,
+    admin_user character varying NOT NULL,
+    admin_password character varying NOT NULL,
+    admin_activo boolean DEFAULT true NOT NULL
+);
+
+
+ALTER TABLE public.admins OWNER TO usuario;
+
+--
+-- Name: admins_admin_id_seq; Type: SEQUENCE; Schema: public; Owner: usuario
+--
+
+CREATE SEQUENCE public.admins_admin_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.admins_admin_id_seq OWNER TO usuario;
+
+--
+-- Name: admins_admin_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: usuario
+--
+
+ALTER SEQUENCE public.admins_admin_id_seq OWNED BY public.admins.admin_id;
+
+
+--
+-- Name: empleados; Type: TABLE; Schema: public; Owner: usuario
+--
+
+CREATE TABLE public.empleados (
+    empleado_id character varying(13) NOT NULL,
+    empleado_password character varying(4) NOT NULL,
+    empleado_activo boolean DEFAULT true NOT NULL,
+    empleado_nombre character varying,
+    empleado_apellido character varying
+);
+
+
+ALTER TABLE public.empleados OWNER TO usuario;
+
+--
+-- Name: empleados_empleado_apellido_seq; Type: SEQUENCE; Schema: public; Owner: usuario
+--
+
+CREATE SEQUENCE public.empleados_empleado_apellido_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.empleados_empleado_apellido_seq OWNER TO usuario;
+
+--
+-- Name: empleados_empleado_apellido_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: usuario
+--
+
+ALTER SEQUENCE public.empleados_empleado_apellido_seq OWNED BY public.empleados.empleado_apellido;
+
+
+--
+-- Name: empleados_empleado_nombre_seq; Type: SEQUENCE; Schema: public; Owner: usuario
+--
+
+CREATE SEQUENCE public.empleados_empleado_nombre_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.empleados_empleado_nombre_seq OWNER TO usuario;
+
+--
+-- Name: empleados_empleado_nombre_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: usuario
+--
+
+ALTER SEQUENCE public.empleados_empleado_nombre_seq OWNED BY public.empleados.empleado_nombre;
+
+
+--
+-- Name: fichajes; Type: TABLE; Schema: public; Owner: usuario
+--
+
+CREATE TABLE public.fichajes (
+    fichaje_id integer NOT NULL,
+    empleado_id character varying NOT NULL,
+    fichaje_es_entrada boolean NOT NULL,
+    fichaje_fecha integer NOT NULL
+);
+
+
+ALTER TABLE public.fichajes OWNER TO usuario;
+
+--
+-- Name: fichajes_fichaje_fecha_seq; Type: SEQUENCE; Schema: public; Owner: usuario
+--
+
+CREATE SEQUENCE public.fichajes_fichaje_fecha_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.fichajes_fichaje_fecha_seq OWNER TO usuario;
+
+--
+-- Name: fichajes_fichaje_fecha_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: usuario
+--
+
+ALTER SEQUENCE public.fichajes_fichaje_fecha_seq OWNED BY public.fichajes.fichaje_fecha;
+
+
+--
+-- Name: fichajes_fichaje_id_seq; Type: SEQUENCE; Schema: public; Owner: usuario
+--
+
+CREATE SEQUENCE public.fichajes_fichaje_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.fichajes_fichaje_id_seq OWNER TO usuario;
+
+--
+-- Name: fichajes_fichaje_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: usuario
+--
+
+ALTER SEQUENCE public.fichajes_fichaje_id_seq OWNED BY public.fichajes.fichaje_id;
+
+
+--
+-- Name: admins admin_id; Type: DEFAULT; Schema: public; Owner: usuario
+--
+
+ALTER TABLE ONLY public.admins ALTER COLUMN admin_id SET DEFAULT nextval('public.admins_admin_id_seq'::regclass);
+
+
+--
+-- Name: fichajes fichaje_id; Type: DEFAULT; Schema: public; Owner: usuario
+--
+
+ALTER TABLE ONLY public.fichajes ALTER COLUMN fichaje_id SET DEFAULT nextval('public.fichajes_fichaje_id_seq'::regclass);
+
+
+--
+-- Name: fichajes fichaje_fecha; Type: DEFAULT; Schema: public; Owner: usuario
+--
+
+ALTER TABLE ONLY public.fichajes ALTER COLUMN fichaje_fecha SET DEFAULT nextval('public.fichajes_fichaje_fecha_seq'::regclass);
+
+
+--
+-- Name: admins admins_admin_user_key; Type: CONSTRAINT; Schema: public; Owner: usuario
+--
+
+ALTER TABLE ONLY public.admins
+    ADD CONSTRAINT admins_admin_user_key UNIQUE (admin_user);
+
+
+--
+-- Name: admins admins_pkey; Type: CONSTRAINT; Schema: public; Owner: usuario
+--
+
+ALTER TABLE ONLY public.admins
+    ADD CONSTRAINT admins_pkey PRIMARY KEY (admin_id);
+
+
+--
+-- Name: empleados empleados_pkey; Type: CONSTRAINT; Schema: public; Owner: usuario
+--
+
+ALTER TABLE ONLY public.empleados
+    ADD CONSTRAINT empleados_pkey PRIMARY KEY (empleado_id);
+
+
+--
+-- Name: fichajes fichajes_pkey; Type: CONSTRAINT; Schema: public; Owner: usuario
+--
+
+ALTER TABLE ONLY public.fichajes
+    ADD CONSTRAINT fichajes_pkey PRIMARY KEY (fichaje_id);
+
+
+--
+-- Name: fichajes fichajes_empleado_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: usuario
+--
+
+ALTER TABLE ONLY public.fichajes
+    ADD CONSTRAINT fichajes_empleado_id_fkey FOREIGN KEY (empleado_id) REFERENCES public.empleados(empleado_id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
